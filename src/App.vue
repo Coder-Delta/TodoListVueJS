f05b1638ac85607fde705994827c9c6dd7709436
Coder-Delta <ranjitbhandary15@gmail.com>
2026-01-09 13:21:25 +0530

    Refactor App.vue to integrate TaskForm component; remove HelloWorld, TheWelcome, WelcomeItem, and icon components; implement task input functionality in TaskForm.

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,12 @@
 <script setup>
-  import { ref } from 'vue';
-  const newTask = ref('');
+import TaskForm from './components/TaskForm.vue';
 </script>
 
 
 <template>
-  <!-- Vue.js provides several built-in directives for common tasks
-  such as conditional rendering, list rendering, event handling, and attribute binding -->
   <main>
-    <h1>Write Your Tasks</h1>
-    <form>
-      <label>
-        New Task: 
-        <input  v-model="newTask" name="newTask">
-      </label>
-      <button type="submit">Add Task</button>
-    </form>
-    <p>{{ newTask }}</p>
+    <TaskForm />
   </main>
 </template>
 
 
-<style scoped>
-  main {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  color: #2c3e50;
-  margin-top: -40px;
-}
-h1 {
-  font-weight: normal;
-}
-</style>
---
+++ src/components/TaskForm.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ref } from 'vue';
+const newTask = ref('');
+
+function addTask() {
+    console.log('Task Added:', newTask.value);
+    newTask.value = '';
+}
+</script>
+
+<template>
+    <!-- Vue.js provides several built-in directives for common tasks
+  such as conditional rendering, list rendering, event handling, and attribute binding -->
+    <main>
+        <h1>Write Your Tasks</h1>
+        <form @submit.prevent="addTask">
+            <label>
+                New Task:
+                <input v-model="newTask" name="newTask">
+            </label>
+            <div>
+                <button type="submit">Add Task</button>
+            </div>
+        </form>
+        <p>{{ newTask }}</p>
+    </main>
+</template>
+
+<style scoped>
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    color: #2c3e50;
+    margin-top: -40px;
+}
+
+h1 {
+    font-weight: normal;
+}
+</style>
